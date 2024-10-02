cef7c1c05e06017ec5d544b587e6a8b0a35f105e
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2024-10-01 20:31:15 -0500

    commit solution for lab 2 - versioning

--- labs/lab2/src/main/java/waa/labs/lab2/controllers/BookController.java
+++ labs/lab2/src/main/java/waa/labs/lab2/controllers/BookController.java
@@ -8,6 +8,7 @@ import waa.labs.lab2.models.Book;
 import waa.labs.lab2.services.IBookService;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -68,5 +69,50 @@ public class BookController {
         }
         bookService.deleteBook(book);
         return new ResponseEntity<>("Delete successfully", HttpStatus.OK);
+    }
+
+    // 6. Add 4 types of versioning.
+    // Media type versioning
+    @GetMapping(value = "/search", produces = "application/v1+json")
+    public List<Book> search(@RequestParam String title) {
+        return bookService.getBooks().stream().filter(b -> b.getTitle().contains(title)).toList();
+    }
+
+    @GetMapping(value = "/search", produces = "application/v2+json")
+    public List<Book> search(@RequestParam double price) {
+        return bookService.getBooks().stream().filter(b -> b.getPrice() >= price).toList();
+    }
+
+    // Header versioning
+    @GetMapping(value = "/search1", headers = "X-API-VERSION=1")
+    public List<Book> search1(@RequestParam String title) {
+        return bookService.getBooks().stream().filter(b -> b.getTitle().contains(title)).toList();
+    }
+
+    @GetMapping(value = "/search1", headers = "X-API-VERSION=2")
+    public List<Book> search1(@RequestParam double price) {
+        return bookService.getBooks().stream().filter(b -> b.getPrice() >= price).toList();
+    }
+
+    // URL versioning
+    @GetMapping("/v1/search2")
+    public List<Book> search2(@RequestParam String title) {
+        return bookService.getBooks().stream().filter(b -> b.getTitle().contains(title)).toList();
+    }
+
+    @GetMapping("/v2/search2")
+    public List<Book> search2(@RequestParam double price) {
+        return bookService.getBooks().stream().filter(b -> b.getPrice() >= price).toList();
+    }
+
+    // Parameter versioning
+    @GetMapping(value = "/list", params = "version=1")
+    public List<Book> getBooks2a() {
+        return bookService.getBooks().stream().sorted(Comparator.comparingDouble(Book::getPrice)).toList();
+    }
+
+    @GetMapping(value = "/list", params = "version=2")
+    public List<Book> getBooks2b() {
+        return bookService.getBooks().stream().sorted(Comparator.comparing(Book::getTitle)).toList();
     }
 }
