aaa77d049a3425e94c8a21547fc4b4824feeac9b
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2024-10-02 19:17:18 -0500

    commit solution for question 1. create a Bidirectional OneToMany association between Department and Employee using annotations.

---
+++ labs/lab3/src/main/java/waa/labs/lab3/onetomany/Department.java
@@ -0,0 +1,25 @@
+package waa.labs.lab3.onetomany;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+// 1. Create a Bidirectional OneToMany association between Department and Employee using annotations.
+@Entity
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // default is SEQUENCE
+    private int id;
+    private String name;
+
+    // OneToMany, default is JoinTable, can skip with mappedBy=...
+    // One Department can have many Employees
+//    @OneToMany
+//    @JoinTable(name = "dept_emp",
+//            joinColumns = @JoinColumn(name = "dept_id"),
+//            inverseJoinColumns = @JoinColumn(name = "emp_id"))
+    @OneToMany(mappedBy = "department")
+    // unless use mappedBy, will have both JoinColumn (department_id in employee table) and JoinTable (dept_emp table)
+    private List<Employee> employees;
+}
