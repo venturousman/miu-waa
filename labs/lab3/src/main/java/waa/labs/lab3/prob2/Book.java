c9477b0eef979a14bc0fef3ac1543205a8f4cd70
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2024-10-02 19:51:22 -0500

    commit solution for problem 2. create an Unidirectional ManyToOne association between Book and Publisher using annotations

---
+++ labs/lab3/src/main/java/waa/labs/lab3/prob2/Book.java
@@ -0,0 +1,22 @@
+package waa.labs.lab3.prob2;
+
+import jakarta.persistence.*;
+
+// 2. Create an Unidirectional ManyToOne association between Book and Publisher using annotations
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // default is SEQUENCE
+    private int id;
+    private String title;
+    @Column(length = 25, nullable = false)
+    private String isbn;
+    private double price;
+
+    // ManyToOne, default is JoinColumn, can use JoinTable, dont have mappedBy
+    @ManyToOne
+//    @JoinTable(name = "book_publisher",
+//            joinColumns = @JoinColumn(name = "book_id"),
+//            inverseJoinColumns = @JoinColumn(name = "publisher_id"))
+    private Publisher publisher;
+}
