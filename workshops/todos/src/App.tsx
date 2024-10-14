c6a569ab5d0c9f839c4a47719fd1e1a6bc45838a
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2024-10-14 10:24:30 -0500

    copy the starter code

--- workshops/todos/src/App.tsx
+++ workshops/todos/src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div id="root">
+            <div className="todo-container">
+                <div className="todo-wrap">
+                    <div className="todo-header">
+                        <input type="text" placeholder="Enter task name"/>
+                    </div>
+                    <ul className="todo-main">
+                        <li>
+                            <label>
+                                <input type="checkbox"/>
+                                <span>xxxxx</span>
+                            </label>
+                            <button className="btn btn-danger" style={{display: 'none'}}>Delete</button>
+                        </li>
+                        <li>
+                            <label>
+                                <input type="checkbox"/>
+                                <span>yyyy</span>
+                            </label>
+                            <button className="btn btn-danger" style={{display: 'none'}}>Delete</button>
+                        </li>
+                    </ul>
+                    <div className="todo-footer">
+                        <label>
+                            <input type="checkbox"/>
+                        </label>
+                        <span>
+                          <span>Finished 0</span> / total 2
+                        </span>
+                        <button className="btn btn-danger">Delete Finished Tasks</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
