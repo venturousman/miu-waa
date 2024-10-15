1a4beae30ee11802a340ac960e2effcc8dcef595
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2024-10-15 11:32:52 -0500

    copy the starter code

--- workshops/users/src/App.tsx
+++ workshops/users/src/App.tsx
@@ -3,12 +3,48 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-            </header>
+        <div id="app">
+            <div className="container">
+                <section className="jumbotron">
+                    <h3 className="jumbotron-heading">Search Github Users</h3>
+                    <div>
+                        <input type="text" placeholder="enter the name you search"/>&nbsp;
+                        <button>Search</button>
+                    </div>
+                </section>
+                <div className="row">
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
